3cbc2e1e25836a564899bfc6469984accf8f5644
Borsas <borsasyt@gmail.com>
2020-08-23 15:11:12 +0300

    9.21 done

--- part9/patientor/frontend/src/PatientPage/Entries.tsx
+++ part9/patientor/frontend/src/PatientPage/Entries.tsx
@@ -1,12 +1,22 @@
 import React from "react";
-import { Entry } from "../types";
+import { Entry, Diagnosis } from "../types";
+import { useStateValue } from "../state";
 
 import { Divider, Header } from "semantic-ui-react";
 
 const Entries: React.FC<{entries: Entry[] | undefined}> = ({entries}) => {
+    const [{ diagnoses }] = useStateValue();
 
     if (!entries) {
         return null;
+    }
+
+    const getDiagnoseName = (code: string):string => {
+        const name = Object.values(diagnoses).find((c:Diagnosis) => c.code === code);
+        if (name === undefined) {
+            return "";
+        }
+        return name.name;
     }
 
     return (
@@ -22,7 +32,9 @@ const Entries: React.FC<{entries: Entry[] | undefined}> = ({entries}) => {
                             </div>
                             <ul>
                                 {entry.diagnosisCodes?.map((code: string) => {
-                                    return <li key={code}>{code}</li>
+                                    return <li key={code}>
+                                        {code} {getDiagnoseName(code)}
+                                        </li>
                                 })}
                             </ul>
                         </div>
--- part9/patientor/frontend/src/state/reducer.ts
+++ part9/patientor/frontend/src/state/reducer.ts
@@ -1,5 +1,5 @@
 import { State } from "./state";
-import { Patient } from "../types";
+import { Patient, Diagnosis } from "../types";
 
 export type Action =
   | {
@@ -13,6 +13,10 @@ export type Action =
   | {
       type: "ADD_CACHE_PATIENT";
       payload: Patient;
+  }
+  | {
+      type: "SET_DIAGNOSES";
+      payload: Diagnosis[];
   };
 
 export const reducer = (state: State, action: Action): State => {
@@ -44,6 +48,17 @@ export const reducer = (state: State, action: Action): State => {
           [action.payload.id]: action.payload
         }
       }
+    case "SET_DIAGNOSES":
+      return {
+        ...state,
+        diagnoses: {
+          ...action.payload.reduce(
+            (memo, diagnose) => ({...memo, [diagnose.code]: diagnose}),
+            {}
+          ),
+          ...state.diagnoses
+        }
+      }
     default:
       return state;
   }
@@ -67,5 +82,12 @@ export const addCachePatient = (newPatient: Patient): Action => {
   return {
     type: "ADD_CACHE_PATIENT",
     payload: newPatient
+  }
+}
+
+export const setDiagnosesList = (diagnosesFromApi: Diagnosis[]): Action => {
+  return {
+    type: "SET_DIAGNOSES",
+    payload: diagnosesFromApi
   }
 }
